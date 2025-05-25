718455d15194b41ae84b909fa2f9617a231984e0
Scranoid <jhabhinav50@gmail.com>
2025-05-25 12:20:56 +0530

    Update Validator.java

--- util/Validator.java
+++ util/Validator.java
@@ -1,5 +1,5 @@
 package util;
-
+//Ive imnported the packages here as well as I've also called the function for the validator class as well
 public class Validator {
     public static boolean isValid(String input) {
         return input != null && !input.trim().isEmpty();
